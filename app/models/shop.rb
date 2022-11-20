e7169aec42b4fc05dda2c77299e18370fcf867b2
axelburtz <axelburtz@gmail.com>
2022-11-20 03:31:29 +0100

    add schedules_order_by_today to sort the schedules by today first / add validations for name and schedule helper in shop model

--- app/models/shop.rb
+++ app/models/shop.rb
@@ -1,4 +1,14 @@
 class Shop < ApplicationRecord
-  has_many :schedules
+  include ScheduleHelper
+
+  validates :name, presence: true
+
+  has_many :schedules, dependent: :destroy
   accepts_nested_attributes_for :schedules, allow_destroy: true
+
+  def schedules_order_by_today
+    days = %w(Monday Tuesday Wednesday Thursday Friday Saturday Sunday)
+    day_offset = days.index(Time.zone.now.strftime("%A")) + 1
+    schedules.order(Arel.sql("(weekday - #{day_offset} + 7) % 7"))
+  end
 end
