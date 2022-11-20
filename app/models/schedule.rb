9e27eceec44723ad4b00743d2f7ef96da12200b1
axelburtz <axelburtz@gmail.com>
2022-11-20 03:30:00 +0100

    add to_weekdays_name method to return the correct name in the view

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,3 +1,24 @@
 class Schedule < ApplicationRecord
   belongs_to :shop
+
+  validates :weekday, presence: true
+
+  def to_weekdays_name
+    case weekday
+    when 1
+      'Monday'
+    when 2
+      'Tuesday'
+    when 3
+      'Wednesday'
+    when 4
+      'Thursday'
+    when 5
+      'Friday'
+    when 6
+      'Saturday'
+    when 7
+      'Sunday'
+    end
+  end
 end
